d91ae83242af6c856ae244b67331b41d80465910
Samiksha <120582351+samiksha-pattir@users.noreply.github.com>
2023-01-03 16:55:31 +0530

    Create aap.js

---
+++ aap.js
@@ -0,0 +1,25 @@
+
+var btn= document.querySelector("#btn-translate");
+var txtInput= document.querySelector("#txt-input");
+
+var outputDiv=document.querySelector("#output");
+var url= "https://api.funtranslations.com/translate/chef.json";
+function getTrans(text){
+    return url+"?"+"text="+text
+}
+
+
+function clickHandler(){
+  fetch(getTrans(txtInput.value))
+    .then (res=> res.json())
+    .then(json=>{
+        var translation=json.contents.translated;
+        outputDiv.innerText=translation;
+    })
+};
+
+
+
+
+btn.addEventListener("click",clickHandler)
+
